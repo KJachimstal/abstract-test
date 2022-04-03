2750abd0f62512d5e1a1602b78dbabf95e067805
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-04-03 20:19:12 +0200

    Header change background when scrolling

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const topButton = document.getElementById("scrollToTopButton");
+const header = document.getElementById("header");
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     topButton.style.display = "flex";
+    header.style.backgroundColor = "#fff";
+    header.style.boxShadow = "0px 0px 10px 3px rgba(0, 0, 0, 0.05)";
   } else {
     topButton.style.display = "none";
+    header.style.backgroundColor = "transparent";
+    header.style.boxShadow = "none";
   }
 }
 
