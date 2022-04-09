5d447ade75481c2dd4a64393d48a1c6204f8a919
Konrad Jachimstal <k.jachimstal@gmail.com>
2022-04-09 22:47:15 +0200

    Add change logo while scrolling

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const topButton = document.getElementById("scrollToTopButton");
 const header = document.getElementById("header");
+const logoLight = document.getElementById("header_logo");
+const logoDark = document.getElementById("header_logo_dark");
 
 window.onscroll = function() {scrollFunction()};
+
+logoDark.style.display = "none";
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     topButton.style.display = "flex";
     header.style.backgroundColor = "#fff";
     header.style.boxShadow = "0px 0px 10px 3px rgba(0, 0, 0, 0.05)";
+    logoLight.style.display = "none";
+    logoDark.style.display = "block";
   } else {
     topButton.style.display = "none";
     header.style.backgroundColor = "transparent";
     header.style.boxShadow = "none";
+    logoLight.style.display = "block";
+    logoDark.style.display = "none";
   }
 }
 
